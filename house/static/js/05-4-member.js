be6e5edf0e42bf688e0d3a226664e94510a6d4c7
sanlazy <446140716@qq.com>
2017-09-29 20:25:46 +0800

    1.0.1
    修改刘海提出的问题和邹腾文档里剩余内容

--- house/static/js/05-4-member.js
+++ house/static/js/05-4-member.js
@@ -5,23 +5,23 @@
     $(document).ready(function() {
 
         //Default Action
-        $(".tab_content").hide(); //Hide all content
-        $("ul.tabs li:first").addClass("active").show(); //Activate first tab
-        $(".tab_content:first").show(); //Show first tab content
-
-        //On Click Event
-        $("ul.tabs li").click(function() {
-            $("ul.tabs li").removeClass("active"); //Remove any "active" class
-            $(this).addClass("active"); //Add "active" class to selected tab
-            $(".tab_content").hide(); //Hide all tab content
-            var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-            $(activeTab).fadeIn(); //Fade in the active content
-            var newHeight = $(activeTab).height();
-            console.log(newHeight);
-            $(".tab_container").css("height",newHeight);
-            $(".tabs").css("height",newHeight);
-            return false;
-        });
+        // $(".tab_content").hide(); //Hide all content
+        // $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+        // $(".tab_content:first").show(); //Show first tab content
+        //
+        // //On Click Event
+        // $("ul.tabs li").click(function() {
+        //     $("ul.tabs li").removeClass("active"); //Remove any "active" class
+        //     $(this).addClass("active"); //Add "active" class to selected tab
+        //     $(".tab_content").hide(); //Hide all tab content
+        //     var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+        //     $(activeTab).fadeIn(); //Fade in the active content
+        //     var newHeight = $(activeTab).height();
+        //     console.log(newHeight);
+        //     $(".tab_container").css("height",newHeight);
+        //     $(".tabs").css("height",newHeight);
+        //     return false;
+        // });
 
 
 
@@ -187,20 +187,7 @@
 
         });
 
-        // tab9 系统消息
-        $(".systInfoTitle").click(function () {
-            if($(this).find("svg use").attr("xlink:href") === "#icon-open"){
-                $(this).find("svg use").attr("xlink:href","#icon-close");
-            }else{
-                $(this).find("svg use").attr("xlink:href","#icon-open");
-            }
 
-            $(this).parent().find(".systInfo").toggle();
-
-            $(".systemInformation").height("auto");
-            $(".tab_container").css("height","auto");
-
-        });
 
     });
 
